e3d81dc1dae4160f6edda5785f9444355c946ce5
Eli Barnett <eeli1@ad.unc.edu>
2021-08-29 13:25:09 -0400

    Working program

---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,11 @@
+"""First try."""
+
+_author_ = 730319741
+
+left_hand_side: int = int(input("Left hand side: "))
+right_hand_side: int = int(input("Right hand side: "))
+
+print(str(left_hand_side) + " < " + str(right_hand_side) + " is " + str(left_hand_side < right_hand_side)) 
+print(str(left_hand_side) + " >= " + str(right_hand_side) + " is " + str(left_hand_side >= right_hand_side))
+print(str(left_hand_side) + " == " + str(right_hand_side) + " is " + str(left_hand_side == right_hand_side))
+print(str(left_hand_side) + " != " + str(right_hand_side) + " is " + str(left_hand_side != right_hand_side))
