6ab6188fa48df5b54e7407b6e7a2a0e86c17ee04
Eli Barnett <eeli1@ad.unc.edu>
2021-09-13 21:20:36 -0400

    Finished

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730319741"
+
+user_string: str = input("Enter a word: ")
+
+i: int = 0
+count: int = 0
+
+while i < len(user_string):
+    letter = user_string[i]
+    index2 = i + 1
+    while index2 < len(user_string): 
+        if user_string[index2] == letter:
+            count = count + 1
+        index2 = index2 + 1
+    i = i + 1
+
+x = count >= 1
+
+print("Found duplicate: " + str(x))
